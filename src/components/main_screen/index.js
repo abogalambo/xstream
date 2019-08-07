ae3ea9f74a3056aa69bcb38b84abce9e13cea870
abogalambo <yaser.al3shry@gmail.com>
2019-08-07 22:45:30 +0200

    use hooks in main screen component

--- src/components/main_screen/index.js
+++ src/components/main_screen/index.js
@@ -1,1 +1,23 @@
-export { default } from './main_screen_container'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useSelector, useDispatch } from 'react-redux';
+import { newStream } from '../../state/actions/stream'
+import Stream from '../stream'
+
+const MainScreen = () => {
+  const currentStream = useSelector(state => state.currentStream);
+  const dispatch = useDispatch();
+  const onNewStreamClick = () => dispatch(newStream())
+
+  return (
+    currentStream ? (
+      <Stream />
+    ) : (
+      <button onClick={onNewStreamClick}> Add New Stream </button>
+    )
+  )
+}
+
+MainScreen.propTypes = {}
+
+export default MainScreen
