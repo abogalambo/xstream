ff6021d8495cede7409105f0cced7b1c005610e8
abogalambo <yaser.al3shry@gmail.com>
2019-08-17 01:46:42 +0200

    add redux-thunk

--- src/home.js
+++ src/home.js
@@ -2,13 +2,19 @@ import 'reset-css';
 import React from "react";
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk';
 import reducer from './state/reducers'
 import App from './components/app'
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer, /* preloadedState, */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
 );
 
 render(
