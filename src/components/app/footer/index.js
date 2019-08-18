82ffb03b722286a1f08fc37c9e09623c2c94fc87
abogalambo <yaser.al3shry@gmail.com>
2019-08-19 00:20:18 +0200

    apply some styles

--- src/components/app/footer/index.js
+++ src/components/app/footer/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStepForward, faStepBackward, faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 import {
   addSegment,
   removeSegment,
@@ -23,14 +25,42 @@ const Footer = () => {
 
   return (
     <div className={styles.footer}>
-      <span> Current Index: { currentSegment.index } </span>
-      <button onClick={onAddSegmentClick}> + </button>
-      <button onClick={onRemoveSegmentClick}> - </button>
-      <button onClick={onPreviousSegmentClick}> {"<"} </button>
-      <button onClick={onNextSegmentClick}> {">"} </button>
+      <div className={styles.player}>
+        <button onClick={onPreviousSegmentClick}>
+          <FontAwesomeIcon className={styles.playerMain_skip}
+            size={'2x'}
+            icon={faStepBackward}/>
+        </button>
+        
+        <button onClick={onNextSegmentClick}>
+          <FontAwesomeIcon className={styles.playerMain_skip}
+            size={'2x'}
+            icon={faStepForward}/>
+        </button>
+
+        <button onClick={onRemoveSegmentClick}>
+          <FontAwesomeIcon className={styles.playerMain_skip}
+            size={'2x'}
+            icon={faMinus}/>
+        </button>
+
+        <button onClick={onAddSegmentClick}>
+          <FontAwesomeIcon className={styles.playerMain_skip}
+            size={'2x'}
+            icon={faPlus}/>
+        </button>
+      </div>
+
       { segment && (
         <Recorder key={`recorder_${segment.timestamp}`} />
       )}
+      <div className={styles.index}>
+        <span>
+          { currentSegment.index > -1 && (
+            `${currentSegment.index + 1} / ${segments.length}`
+          )}
+        </span>
+      </div>
     </div>
   )
 }
