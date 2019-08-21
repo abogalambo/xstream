85fafcfe588f282ddcb76d4e08cd328d97eea82d
abogalambo <yaser.al3shry@gmail.com>
2019-08-22 00:10:08 +0200

    style segment with multiple inputs

--- src/components/app/segment/index.js
+++ src/components/app/segment/index.js
@@ -1,47 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useDispatch } from 'react-redux';
-import classnames from 'classnames'
+import classnames from 'classnames';
 import { setSegmentText } from '../../../state/actions/segment'
 import TextInput from '../../lib/text_input'
 import styles from './segment.css'
 
 const Segment = ({
-  text,
-  current,
-  previous,
-  next
+  text
 }) => {
   const dispatch = useDispatch();
   const onTextChange = (event) => {
-    current && dispatch(setSegmentText(event.target.value))
+    dispatch(setSegmentText(event.target.value))
   }
 
   return (
-    <div className={classnames(
-      styles.segment,
-      {
-        [styles.current]: current,
-        [styles.previous]: previous,
-        [styles.next]: next
-      }
-    )}>
-      <TextInput
-        value={text || ''}
-        onChange={onTextChange}
-        maxChars={200}
-        prompt="Write Something .."
-        disabled={!current}
-      />
+    <div className={styles.segment}>
+      <div className={classnames(
+        styles.mediaInput,
+        {
+          [styles.collapsed]: false
+        }
+      )}>
+        <TextInput
+          value={text || ''}
+          onChange={onTextChange}
+          maxChars={200}
+          prompt="Write Something .."
+        />
+      </div>
+      <div className={classnames(
+        styles.mediaInput,
+        {
+          [styles.collapsed]: !!text
+        }
+      )}>
+      </div>
     </div>
   )
 }
 
 Segment.propTypes = {
-  text: PropTypes.string,
-  current: PropTypes.bool,
-  previous: PropTypes.bool,
-  next: PropTypes.bool
+  text: PropTypes.string
 }
 
 export default Segment
