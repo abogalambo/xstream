6739173d388507b64d1e993d4a82d8cad5acf438
abogalambo <yaser.al3shry@gmail.com>
2019-08-28 00:23:30 +0200

    render Cover component in playback mode

--- src/components/lib/text_input/index.js
+++ src/components/lib/text_input/index.js
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import styles from './text_input.css'
 
-const TextInput = ({ value, onChange, maxChars, prompt, autoFocus, disabled }) => (
+const TextInput = ({ value, onChange, maxChars, prompt, readOnly }) => (
   <textarea
     className={classnames(styles.textDisplay, styles.textInput)}
     value={value}
-    onChange={onChange}
-    maxLength={maxChars}
-    placeholder={prompt}
-    autoFocus={autoFocus}
-    disabled={disabled}
+    disabled={readOnly}
+    {...(!readOnly && {
+      onChange,
+      maxLength: maxChars,
+      placeholder: prompt
+    })}
   />
 )
 
@@ -20,8 +21,7 @@ TextInput.propTypes = {
   onChange: PropTypes.func,
   maxChars: PropTypes.number,
   prompt: PropTypes.string,
-  autoFocus: PropTypes.bool,
-  disabled: PropTypes.bool
+  readOnly: PropTypes.bool
 }
 
 export default TextInput
