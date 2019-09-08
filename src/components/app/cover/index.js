1a4a87c004277c58007321904320d165d198b793
abogalambo <yaser.al3shry@gmail.com>
2019-09-08 13:54:02 +0200

    enable removing the cover image

--- src/components/app/cover/index.js
+++ src/components/app/cover/index.js
@@ -3,7 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPlus,
-  faPlay
+  faPlay,
+  faTimes
 } from '@fortawesome/free-solid-svg-icons'
 import {
   addSegment,
@@ -33,12 +34,14 @@ const Cover = () => {
   const startTyping = () => dispatch(startTypingAction())
   const stopTyping = () => dispatch(stopTypingAction())
   const addCoverImage = (e) => dispatch(addCoverImageAction(e))
+  const removeCoverImage = () => dispatch(removeCoverImageAction())
 
   const icon = (hasSegments || isPlaybackMode) ? faPlay : faPlus
   const action = (hasSegments || isPlaybackMode) ? goToSegment(0) : addSegment()
   const onClick = () => dispatch(action)
 
-  const coverStyle = (coverImage && coverImage.src) ? {
+  const hasCoverImage = coverImage && coverImage.src
+  const coverStyle = hasCoverImage ? {
     backgroundImage: `url(${coverImage.src})`
   } : {}
 
@@ -70,11 +73,23 @@ const Cover = () => {
       </button>
 
       {!isPlaybackMode && (
-        <div className={styles.addImage}>
-          <ImageInput
-            onChange={addCoverImage}
-            buttonDisplay
-          />
+        <div className={styles.addRemoveImage}>
+          {hasCoverImage ? (
+            <button
+              className={styles.removeImage}
+              onClick={removeCoverImage}
+            >
+              <FontAwesomeIcon
+                size={'2x'}
+                icon={faTimes}
+              />
+            </button>
+          ) : (
+            <ImageInput
+              onChange={addCoverImage}
+              buttonDisplay
+            />
+          )}
         </div>
       )}
     </div>
