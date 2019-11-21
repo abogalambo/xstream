a3c68ea6b59391148dfa10364f3bc6b8312682fc
Yaser Alashry <yaser.alashry@xing.com>
2019-11-21 15:44:18 +0100

    enable adding segments from the segments overview

--- src/components/app/segments_overview/index.js
+++ src/components/app/segments_overview/index.js
@@ -1,23 +1,41 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
-import classnames from 'classnames';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import {
+  addSegment,
+  removeSegment,
+  goToSegment,
+} from '../../../state/actions/stream'
 
 import {
+  segmentsSelector,
   isPlaybackModeSelector
 } from '../../../state/selectors/current_stream'
 
 import styles from './segments_overview.css'
 
 const SegmentsOverview = () => {
+  const segments = useSelector(segmentsSelector)
   const isPlaybackMode = useSelector(isPlaybackModeSelector)
 
+  const dispatch = useDispatch();
+  const onAddSegmentClick = () => dispatch(addSegment())
+
   return (
-    <div className={classnames(
-        styles.segmentsOverview,
-        {
-          [styles.collapsed]: isPlaybackMode
-        }
-    )}>
+    <div className={styles.segmentsOverview}>
+      {!isPlaybackMode && (
+        <button
+          onClick={onAddSegmentClick}
+          className={styles.addSegmentButton}
+        >
+          <FontAwesomeIcon
+            size={'2x'}
+            icon={faPlus}
+          />
+        </button>
+      )}
     </div>
   )
 }
