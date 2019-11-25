0e6e8c858c6a3b0326cd3aa601f9f9284377e434
abogalambo <yaser.al3shry@gmail.com>
2019-11-25 03:32:06 +0100

    refined autosave logic

--- src/components/app/autosave/index.js
+++ src/components/app/autosave/index.js
@@ -7,11 +7,13 @@ const Autosave = () => {
   
   useEffect(() => {
     if(lastUpdateAt != null) {
-      if(lastRequestTriggeredAt < lastUpdateAt) {
-        const currentTime = (new Date).getTime()
-        if(lastRequestTriggeredAt < currentTime - config.stream.autosaveInterval) {
-          if(lastRequestStatus != 'pending') {
+      if(lastRequestTriggeredAt < lastUpdateAt) { // user made an update after last server update
+        if(lastRequestStatus != 'pending') { // there is an update request pending, wait for it then this code will trigger again
+          const currentTime = (new Date).getTime()
+          if(lastRequestTriggeredAt < currentTime - config.stream.autosaveInterval) {
             console.log('need to update server')
+          }else{
+            console.log('need to make a delayed update to server')
           }
         }
       }
