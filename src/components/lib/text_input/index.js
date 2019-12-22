363c94ded3df71134fca470b10557ab4520f5a80
abogalambo <yaser.al3shry@gmail.com>
2019-12-22 03:52:44 +0100

    select the text area text when switching to editing

--- src/components/lib/text_input/index.js
+++ src/components/lib/text_input/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import styles from './text_input.css'
@@ -6,6 +6,16 @@ import styles from './text_input.css'
 const TextInput = ({ value, minSize = 5, maxSize = 10, onChange, onFocus, onBlur, maxChars, prompt, readOnly }) => {
 
   const [isEditing, setIsEditing] = useState(false)
+
+  const textAreaRef = useRef(null)
+
+  useEffect(() => {
+    const el = textAreaRef.current
+    if(isEditing) {
+      el.focus()
+      el.setSelectionRange(el.value.length, el.value.length)
+    }
+  }, [isEditing])
 
   const handleOnFocus = (e) => {
     setIsEditing(true)
@@ -17,7 +27,7 @@ const TextInput = ({ value, minSize = 5, maxSize = 10, onChange, onFocus, onBlur
     onBlur && onBlur(e)
   }
 
-  const handleOnClick = () => {
+  const handleOnClick = (e) => {
     setIsEditing(true)
   }
 
@@ -45,6 +55,7 @@ const TextInput = ({ value, minSize = 5, maxSize = 10, onChange, onFocus, onBlur
               [styles.hidden]: hideTextArea
             }
           )}
+          ref={textAreaRef}
           value={value}
           disabled={readOnly}
           wrap="hard"
