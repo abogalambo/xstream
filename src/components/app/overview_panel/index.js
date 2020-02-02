76100a924bc457d0a7c587a3e82272bbdbe70e0e
abogalambo <yaser.al3shry@gmail.com>
2020-02-02 03:58:50 +0100

    simplify overview panel

--- src/components/app/overview_panel/index.js
+++ src/components/app/overview_panel/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { SortableContainer } from 'react-sortable-hoc'
 import {
   reorderSegments
 } from '../../../state/actions/stream'
-import {
-  isPlaybackModeSelector,
-} from '../../../state/selectors/current_stream'
 import PlainOverviewPanel from './plain_overview_panel'
 import styles from './overview_panel.css'
 
@@ -14,15 +11,12 @@ const SortableOverviewPanel = SortableContainer(PlainOverviewPanel)
 
 const OverviewPanel = () => {
   const dispatch = useDispatch()
-  const isPlaybackMode = useSelector(isPlaybackModeSelector)
   const onSortEnd = ({oldIndex, newIndex}) => {
     if(oldIndex === newIndex) return
     dispatch(reorderSegments(oldIndex, newIndex))
   }
 
-  return isPlaybackMode ? (
-    <PlainOverviewPanel />
-  ) : (
+  return (
     <SortableOverviewPanel
       onSortEnd={onSortEnd}
       helperClass={styles.draggedSegment}
