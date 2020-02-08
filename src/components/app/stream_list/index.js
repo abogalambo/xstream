6153f04da73c56c057bd3f7ef822d52a890e5c9b
abogalambo <yaser.al3shry@gmail.com>
2020-02-08 16:59:48 +0100

    style stream list

--- src/components/app/stream_list/index.js
+++ src/components/app/stream_list/index.js
@@ -1,27 +1,44 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
 import {
   streamsSelector
 } from '../../../state/selectors/current_stream_list'
+import ImageDisplay from '../../lib/image_display'
+import defaultCoverImage from '../../../img/default_cover.jpg'
+import styles from './stream_list.css'
 
 const StreamList = () => {
   const streams = useSelector(streamsSelector)
 
   return (
-    <>
+    <div className={styles.streamList}>
       { streams.map((stream) => {
         const { id, cover, title } = stream
 
         return (
-          <div>
-            { cover && (
-              <img src={cover.src} />
-            )}
-            <h1> { title } </h1>
-          </div>
+          <a
+            href={`/streams/${id}`}
+            className={styles.listItem}
+            key={`stream_thumbnail_${id}`}
+          >
+            <a
+              className={styles.editLink}
+              href={`/streams/${id}/edit`}
+            >
+              <FontAwesomeIcon icon={faPen}/>
+            </a>
+            <ImageDisplay
+              src={cover && cover.src ? cover.src : defaultCoverImage}
+              caption={title || "Untitled Stream"}
+              style="COVER"
+              editable={false}
+            />
+          </a>
         )
       })}
-    </>
+    </div>
   )
 }
 
