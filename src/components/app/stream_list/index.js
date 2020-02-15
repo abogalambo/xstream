1ed1e6b26a86036903170d6dbc95ec33091d6d53
abogalambo <yaser.al3shry@gmail.com>
2020-02-15 17:02:56 +0100

    fix lint errors

--- src/components/app/stream_list/index.js
+++ src/components/app/stream_list/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import {
   streamsSelector
 } from '../../../state/selectors/current_stream_list'
