d7f10669f7b11c2f967f5bf3d4c2100dd31676f3
abogalambo <yaser.al3shry@gmail.com>
2020-04-12 12:46:24 +0200

    add publish button to compose bar

--- src/components/app/compose_bar/index.js
+++ src/components/app/compose_bar/index.js
@@ -1,21 +1,40 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { publishStream as publishStreamAction } from '../../../state/actions/stream'
+import {
+  currentStreamSelector,
+  canPublishStreamSelector,
+  streamPublishedAtSelector
+} from '../../../state/selectors/current_stream'
 import RemainingTime from '../remaining_time'
 import ModeToggle from '../mode_toggle'
-
 import styles from './compose_bar.css'
 
-const ComposeBar = () => (
-  <header className={styles.header}>
+const ComposeBar = () => {
+  const { id } = useSelector(currentStreamSelector) || {}
+  const dispatch = useDispatch()
+  const publishStream = () => { dispatch(publishStreamAction(id)) }
+  const canPublishStream = useSelector(canPublishStreamSelector)
+  const streamPublishedAt = useSelector(streamPublishedAtSelector)
 
-    <div className={styles.topBar}>
-      <div className={styles.remainingTimeContainer}>
-        <RemainingTime />
+  return (
+    <header className={styles.header}>
+      <div className={styles.topBar}>
+        <button
+          onClick={publishStream}
+          disabled={!canPublishStream}
+        >
+          {streamPublishedAt ? 'Published!' : 'Publish'}
+        </button>
+        <div className={styles.remainingTimeContainer}>
+          <RemainingTime />
+        </div>
+        <div className={styles.toggleBtnContainer}>
+          <ModeToggle />
+        </div>
       </div>
-      <div className={styles.toggleBtnContainer}>
-        <ModeToggle />
-      </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 export default ComposeBar
