02e9758b037f7c8bf7b6967c12eaf40f9417b363
abogalambo <yaser.al3shry@gmail.com>
2020-04-26 01:57:20 +0200

    identify links and make them clickable

--- src/components/lib/text_input/index.js
+++ src/components/lib/text_input/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
+import urlRegex from 'url-regex'
 import styles from './text_input.css'
 
 const TextInput = ({ value, minSize = 5, maxSize = 10, onChange, onFocus, onBlur, maxChars, prompt, readOnly }) => {
@@ -11,10 +12,13 @@ const TextInput = ({ value, minSize = 5, maxSize = 10, onChange, onFocus, onBlur
 
   useEffect(() => {
     const el = textAreaRef.current
-    if(value != el.innerText) {
-      resetText()
+    if(el) {
+      if(value != el.innerText) {
+        resetText()
+      }
+      el.focus()
     }
-  }, [value])
+  }, [value, isEditing])
 
   const handleOnFocus = (e) => {
     setIsEditing(!readOnly)
@@ -53,30 +57,74 @@ const TextInput = ({ value, minSize = 5, maxSize = 10, onChange, onFocus, onBlur
     sel.addRange(range);
   }
 
+  const convertLinks = () => {
+    const words = value.split(' ')
+
+    let nodes = []
+    let nonLinks = []
+
+    words.forEach((word, index) => {
+      if(urlRegex({strict: false}).test(word)) {
+        if(nonLinks.length > 0) {
+          nodes.push(nonLinks.join(" "))
+          nonLinks = []
+        }
+
+        const url = word.toLowerCase().startsWith("http") ? word : `//${word}`
+
+        nodes.push(
+          <a href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            &nbsp;{word}&nbsp;
+          </a>
+        )
+
+      } else {
+        nonLinks.push(word)
+        if(index == words.length - 1) {
+          nodes.push(nonLinks.join(" "))
+          nonLinks = []
+        }
+      }
+    })
+
+    return nodes
+  }
+
   return (
-    <div className={styles.wrapper}>
+    <div
+      className={styles.wrapper}
+      onClick={handleOnClick}
+    >
       {!isEditing && !value && (
         <div className={styles.prompt}>{prompt}</div>
       )}
 
-      <div
-        className={classnames(
-          styles.textDisplay,
-          styles.textInput,
-          {
-            [styles.empty]: !value && isEditing            
-          }
-        )}
-        onClick={handleOnClick}
-        ref={textAreaRef}
-        contentEditable={!readOnly}
-        style={fontStyle(value, minSize, maxSize, maxChars)}
-        onFocus={handleOnFocus}
-        onBlur={handleOnBlur}
-        maxLength={maxChars}
-        placeholder={prompt}
-        onInput={handleInput}
-      />
+      {!isEditing && (
+        <div
+          className={classnames(styles.textDisplay)}
+          style={fontStyle(value, minSize, maxSize, maxChars)}
+        >
+          { convertLinks() }
+        </div>
+      )}
+
+      {isEditing && (
+        <div
+          className={classnames(
+            styles.textDisplay,
+            styles.textInput
+          )}
+          ref={textAreaRef}
+          contentEditable
+          style={fontStyle(value, minSize, maxSize, maxChars)}
+          onFocus={handleOnFocus}
+          onBlur={handleOnBlur}
+          onInput={handleInput}
+        />
+      )}
 
       {isEditing && (
         <span>{maxChars - (value || "").length}</span>
