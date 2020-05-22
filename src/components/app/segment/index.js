25a5cbc2ba9a2f4cbd95964a27b81f8bc60c8f2e
abogalambo <yaser.al3shry@gmail.com>
2020-05-22 04:19:54 +0200

    clean up compose logic from segment component

--- src/components/app/segment/index.js
+++ src/components/app/segment/index.js
@@ -1,12 +1,47 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  currentSegmentDataSelector
+} from '../../../state/selectors/current_stream'
+import {
+  pauseStream
+} from '../../../state/actions/playback'
+import TextInput from '../../lib/text_input'
+import SegmentImage from '../segment_image'
+import ClosedCaption from '../closed_caption'
 import PlaybackPlayer from '../playback_player'
-import PlainSegment from './segment'
+import styles from './segment.css'
+import config from '../../../../config'
 
 const Segment = () => {
+  const { text, image } = useSelector(currentSegmentDataSelector)
+
+  const dispatch = useDispatch();
+
+  const onLinkClick = () => {
+    dispatch(pauseStream())
+  }
 
   return (
     <PlaybackPlayer>
-      <PlainSegment />
+      <div className={styles.segment}>
+        {text && (
+          <TextInput
+            value={text}
+            minSize={5}
+            maxSize={5}
+            onLinkClick={onLinkClick}
+            maxChars={config.stream.text.maxLength}
+            readOnly
+          />
+        )}
+
+        {image && (
+          <SegmentImage image={image} />
+        )}
+        
+        <ClosedCaption />
+      </div>
     </PlaybackPlayer>
   )
 }
